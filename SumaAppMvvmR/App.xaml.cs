8fae061fac49201998b06319580a07c98a194886
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-06-14 18:31:28 -0600

    Agregar archivos de proyecto.

---
+++ SumaAppMvvmR/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SumaAppMvvmR
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SumaAppMvvmR/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SumaAppMvvmR
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
